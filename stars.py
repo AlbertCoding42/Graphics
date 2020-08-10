fc6d89e0b847c3fa0a19068a7a5dfd46320c6a39
Bertie <69463200+AlbertCoding42@users.noreply.github.com>
2020-08-10 13:14:19 +0100

    Star Pattern
    Draws a star pattern.

---
+++ stars.py
@@ -0,0 +1,32 @@
+import turtle
+
+
+bob = turtle.Turtle()
+bob.getscreen().bgcolor("#ABEBC6")
+bob.color("red")
+bob.speed(0)
+bob.penup()
+bob.goto((-200),(100))
+bob.pendown()
+bob.speed(10)
+
+# Draw stars:
+
+def stars(turtle, size):
+    if size < 25:
+        return
+    else:
+        for i in range(5):
+           turtle.forward(size)
+           stars(bob, size / 3)
+           turtle.left(216)
+
+
+stars(bob, 300)
+
+
+
+
+
+
+turtle.done()
